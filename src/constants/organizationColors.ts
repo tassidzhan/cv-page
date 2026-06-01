74903675bba8c16d2b4ab4d6377d4f08e5b690ce
tassidzhan <mankeliska@gmail.com>
2026-06-02 01:29:27 +0300

    Update organizationColors.ts

--- src/constants/organizationColors.ts
+++ src/constants/organizationColors.ts
@@ -14,7 +14,7 @@ export const ORGANIZATION_COLORS: Record<string, string> = {
   Athene: '#384a28',
   AcademiaStudentica: '#0313ff',
   GAYY: '#ff03af',
-  Museo: '#00a663'
+  Museo: '#00a663',
 };
 
 /**
